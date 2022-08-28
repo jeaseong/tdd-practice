a11aafd2ab40bf382e453717686235d95660b801
jsPark <wotjddnfl@gmail.com>
2022-08-28 18:24:21 +0900

    chore: 최종 화면 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,13 @@
-import React, { useState } from "react";
-import "./App.css";
+import React from "react";
+import { OrderProvider } from "./orderTest/pages/orderPage/contexts/OrderContext";
+import Order from "./orderTest/pages/orderPage/Order";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [disable, setDisable] = useState(false);
-  const onClickPlus = () => {
-    setCounter((cur) => cur + 1);
-  };
-  const onClickMinus = () => {
-    setCounter((cur) => cur - 1);
-  };
-  const onClickDisableBtn = () => {
-    setDisable((cur) => !cur);
-  };
   return (
-    <div className="App">
-      <h3 data-testid="counter">{counter}</h3>
-      <button disabled={disable} data-testid="minusBtn" onClick={onClickMinus}>
-        -
-      </button>
-      <button disabled={disable} data-testid="plusBtn" onClick={onClickPlus}>
-        +
-      </button>
-      <button
-        style={{ backgroundColor: "blue" }}
-        data-testid="onOffBtn"
-        onClick={onClickDisableBtn}
-      ></button>
+    <div>
+      <OrderProvider>
+        <Order />
+      </OrderProvider>
     </div>
   );
 }
--- src/orderTest/pages/orderPage/Order.tsx
+++ src/orderTest/pages/orderPage/Order.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Type from "./type/Type";
 
 function Order() {
-  return <div>Order</div>;
+  return (
+    <div>
+      <h1>Travel Products</h1>
+      <div>
+        <Type orderType="products" />
+      </div>
+      <div style={{ display: "flex", marginTop: 20 }}>
+        <div style={{ width: "50%" }}>{/* <Type orderType="options" /> */}</div>
+        <div>
+          <h2>Total Price: </h2>
+          <button>주문</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Order;
